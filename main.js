431ee63c7c6b987ec8c6631260fab2543da7973b
C21IO1049 <c21io1049@ufe.edu.mn>
2022-03-31 23:09:14 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,86 @@
+const mysql = require('mysql');
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+
+const connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : 'Tushig123t',
+	database : 'nodelogin'
+});
+
+const app = express();
+
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname + '/Homepage.html'));
+});
+
+app.post('/tolgn', function(request, response){
+	response.sendFile(path.join(__dirname + '/login.html'));
+});
+
+app.post('/auth', function(request, response) {
+	let username = request.body.username;
+	let password = request.body.password;
+	if (username && password) {
+		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			if (error) throw error;
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.username = username;
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+// http://localhost:3000/home
+app.get('/home', function(request, response) {
+	// If the user is loggedin
+	if (request.session.loggedin) {
+		// Output username
+		response.send('Welcome back, ' + request.session.username + '!');
+	} else {
+		// Not logged in
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
+
+app.post('/toregister', function(request, response){
+	response.sendFile(path.join(__dirname + '/Register.html'));
+});
+
+app.post('/registeract', function(request, response){
+    let uname = request.body.uname;
+    let pword = request.body.pword;
+    let email = request.body.email;;
+    if(uname && pword && email ){
+        connection.query('INSERT INTO accounts (username, password, email) VALUES (?,?,?)',[uname,pword,email], function(error, results, fields){
+            if(error){
+                console.log('Reg failed');
+            }else{
+                console.log('Reg successfull')
+            }
+        });
+    }
+    response.end();
+});
+
+app.listen(3000);
